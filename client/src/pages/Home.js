8aac2f1d399e6c5f665f4ca232e869766d487892
Nicat Rzayev <99348762+RzayevNicat@users.noreply.github.com>
2023-01-20 18:17:39 +0400

    Add files via upload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,124 +1,128 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import axios from 'axios'
-import { useDetails } from '../context/Contextt'
-
+import axios from 'axios';
+import { useDetails } from '../context/Contextt';
 
 function Home() {
-    const { details } = useDetails();
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:3000/products')
-            .then((response) => response.json())
-            .then((data) => setData(data));
+	const { details } = useDetails();
+	const [ data, setData ] = useState([]);
+	const datas = Object.values(data);
+	console.log(datas);
+	useEffect(() => {
+		axios.get('http://localhost:3000/products').then((res) => setData(res.data));
+	}, []);
+	const handleDelete = (id) => {
+		axios.delete(`http://localhost:3000/products/${id}`);
+		const copy = data.filter((x) => x._id !== id);
+		setData(copy);
+	};
 
-    }, [])
-    const handleDelete = (id) => {
-        axios.delete(`http://localhost:3000/products/${id}`)
-            .then(res => setData(res.data))
-
-    }
-    const datas = Object.values(data);
-
-    function search(items) {
-
-
-        return items.filter((item) => item.productName.toString().toLowerCase().indexOf(details.toLowerCase()) > -1);
-
-    }
-    return (
-        <div>
-            <header>
-                <h1>Notary Public & Legal Solutions</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis cupiditate deserunt placeat necessitatibus a aliquam corrupti nisi odio aliquid, accusamus.</p>
-            </header>
-            <section className='cards'>
-                <h1>Practice <span>Areas</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <div className='card'>
-                    {
-                        search(datas).map((ele, index) => (
-                            <Card className='product' sx={{ maxWidth: 345 }} key={index}>
-                                <CardMedia
-                                    sx={{ height: 140 }}
-                                    image={ele.src}
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Product Name: {ele.productName}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Price:  {ele.price}
-                                    </Typography>
-                                    <CardActions>
-                                        <Button size="small" onClick={() => handleDelete(ele.id)}>Delete</Button>
-                                    </CardActions>
-                                </CardContent>
-                            </Card>
-                        ))
-                    }
-                </div>
-            </section>
-            <section className='reliable'>
-                <div className='images'>
-                    <img src='https://preview.colorlib.com/theme/notary/images/atty_1.jpg.webp' />
-                    <img src='https://preview.colorlib.com/theme/notary/images/atty_2.jpg.webp' />
-
-                </div>
-                <div className='info'>
-                    <h1>We Provide Highly <span>Reliable & Effective</span> Legal Solutions</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, explicabo iste a labore id est quas, doloremque veritatis! Provident odit pariatur dolorem quisquam, voluptatibus voluptates optio accusamus, vel quasi quidem!</p>
-                    <button>Book an apointment</button>
-                </div>
-            </section>
-            <section className='customers'>
-                <h1>Happy Customers</h1>
-                <div className='customerCards'>
-                    <div className='card1'>
-                        <div className='customer-img'>
-                            <img src='https://preview.colorlib.com/theme/notary/images/person_2.jpg.webp' />
-                            <div className='customer-info'>
-                                <h5>Lina Gold</h5>
-                                <p>Owner Ford</p>
-                            </div>
-                        </div>
-                        <div className='customer-about'>
-                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"
-                        </div>
-                    </div>
-                    <div className='card1'>
-                        <div className='customer-img'>
-                            <img src='https://preview.colorlib.com/theme/notary/images/person_1.jpg.webp' />
-                            <div className='customer-info'>
-                                <h5>Lina Gold</h5>
-                                <p>Owner Ford</p>
-                            </div>
-                        </div>
-                        <div className='customer-about'>
-                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"
-                        </div>
-                    </div>
-                    <div className='card1'>
-                        <div className='customer-img'>
-                            <img src='https://preview.colorlib.com/theme/notary/images/person_3.jpg.webp' />
-                            <div className='customer-info'>
-                                <h5>Lina Gold</h5>
-                                <p>Owner Ford</p>
-                            </div>
-                        </div>
-                        <div className='customer-about'>
-                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet veniam. Ipsam, nam, voluptatum"
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    )
+	function search(items) {
+		return items.filter((item) => item.productName.toLowerCase().indexOf(details.toLowerCase()) > -1);
+	}
+	return (
+		<div>
+			<header>
+				<h1>Notary Public & Legal Solutions</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis cupiditate deserunt placeat
+					necessitatibus a aliquam corrupti nisi odio aliquid, accusamus.
+				</p>
+			</header>
+			<section className="cards">
+				<h1>
+					Practice <span>Areas</span>
+				</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<div className="card">
+					{search(datas).map((ele, index) => (
+						<Card className="product" sx={{ maxWidth: 345 }} key={index}>
+							<CardMedia sx={{ height: 140 }} image={ele.src} />
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="div">
+									Product Name: {ele.productName}
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
+									Price: {ele.price}
+								</Typography>
+								<CardActions>
+									<Button size="small" onClick={() => handleDelete(ele._id)}>
+										Delete
+									</Button>
+								</CardActions>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			</section>
+			<section className="reliable">
+				<div className="images">
+					<img src="https://preview.colorlib.com/theme/notary/images/atty_1.jpg.webp" />
+					<img src="https://preview.colorlib.com/theme/notary/images/atty_2.jpg.webp" />
+				</div>
+				<div className="info">
+					<h1>
+						We Provide Highly <span>Reliable & Effective</span> Legal Solutions
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, explicabo iste a labore
+						id est quas, doloremque veritatis! Provident odit pariatur dolorem quisquam, voluptatibus
+						voluptates optio accusamus, vel quasi quidem!
+					</p>
+					<button>Book an apointment</button>
+				</div>
+			</section>
+			<section className="customers">
+				<h1>Happy Customers</h1>
+				<div className="customerCards">
+					<div className="card1">
+						<div className="customer-img">
+							<img src="https://preview.colorlib.com/theme/notary/images/person_2.jpg.webp" />
+							<div className="customer-info">
+								<h5>Lina Gold</h5>
+								<p>Owner Ford</p>
+							</div>
+						</div>
+						<div className="customer-about">
+							"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet
+							veniam. Ipsam, nam, voluptatum"
+						</div>
+					</div>
+					<div className="card1">
+						<div className="customer-img">
+							<img src="https://preview.colorlib.com/theme/notary/images/person_1.jpg.webp" />
+							<div className="customer-info">
+								<h5>Lina Gold</h5>
+								<p>Owner Ford</p>
+							</div>
+						</div>
+						<div className="customer-about">
+							"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet
+							veniam. Ipsam, nam, voluptatum"
+						</div>
+					</div>
+					<div className="card1">
+						<div className="customer-img">
+							<img src="https://preview.colorlib.com/theme/notary/images/person_3.jpg.webp" />
+							<div className="customer-info">
+								<h5>Lina Gold</h5>
+								<p>Owner Ford</p>
+							</div>
+						</div>
+						<div className="customer-about">
+							"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, deserunt eveniet
+							veniam. Ipsam, nam, voluptatum"
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
--- server/index.js
+++ server/index.js
@@ -1,112 +1,98 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = express();
 
-const app = express()
-const PORT = 3000
+dotenv.config();
 
-let idCounter = 5
+const { Schema } = mongoose;
 
-let products = [
-    {
-        id: 1,
-        src: 'https://saglamolun.az/images/stories/Maraqlixeber/kolbasa_nitrit.jpg',
-        productName: 'Kolbasa',
-        price: '20$',
-        count: 10
-    },
-    {
-        id: 2,
-        src: 'https://bazarstore.az/16170-large_default/sems-sosis-kq.jpg',
-        productName: 'Sosiska',
-        price: '10$',
-        count: 6
-    },
-    {
-        id: 3,
-        src: 'https://d3i7mz61kz7ozx.cloudfront.net/prod/uploads/image/file/extra_large_b937eea642_1526974944.jpg',
-        productName: 'Pendir',
-        price: '15$',
-        count: 7
-    },
-    {
-        id: 4,
-        src: 'https://tereyagi.gen.tr/images/tereyagi-kac-kalori.jpg',
-        productName: 'Yag',
-        price: '20$',
-        count: 30
-    },
-    {
-        id: 5,
-        src: 'https://yuz.uz/imageproxy/1200x/https://yuz.uz/file/news/6386427e42cd8f7709bc39255247d705.jpg',
-        productName: 'Shokolad',
-        price: '20$',
-        count: 10
-    },
-    {
-        id: 6,
-        src: 'https://i.ytimg.com/vi/xXA9jlMQZpM/maxresdefault.jpg',
-        productName: 'Shor',
-        price: '2$',
-        count: 30
-    }
-]
+const productSchema = new Schema(
+	{
+		productName: { type: String, required: true },
+		price: { type: String, required: true },
+		count: { type: Number, required: true },
+		src: { type: String, required: true }
+	},
+	{ timestamps: true }
+);
 
-app.use(cors())
-app.use(bodyParser.json())
-app.get("/", (req, res) => {
-    res.send("<h1>My dataa</h1>")
-})
-app.get("/products", (req, res) => {
-    res.send(products)
-})
+const Products = mongoose.model('products', productSchema);
 
-app.get("/products/:id", (req, res) => {
-    const id = req.params.id
-    selectProduct = products.find(x => x.id == id)
-    if (selectProduct) {
-        res.send(selectProduct)
-        res.status(200).json({ message: 'Ok' })
-    }
-    else {
-        res.status(404).json({ message: 'Not Found' })
-    }
-})
-app.delete("/products/:id", (req, res) => {
-    const id = req.params.id
-    products = products.filter(x => x.id != id)
-    res.send(products)
-    res.status(200).json({ message: 'Ok' })
-})
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+	res.send('<h1>My dataa</h1>');
+});
+app.get('/products', (req, res) => {
+	Products.find({}, (err, docs) => {
+		if (!err) {
+			res.send(docs);
+		} else {
+			res.status(404).json({ message: err });
+		}
+	});
+});
 
-app.post("/products", (req, res) => {
-    let productss = {
-        id: idCounter++,
-        productName: req.body.productName,
-        price: req.body.price,
-        count: req.body.count,
-        src: req.body.src
-    }
-    products.push(productss)
-    res.send(products)
-})
+app.get('/products/:id', (req, res) => {
+	const { id } = req.params;
+	Products.findById(id, (err, doc) => {
+		if (!err) {
+			if (doc) {
+				res.send(doc);
+			} else {
+				res.status(404).json({ message: 'NOT FOUND' });
+			}
+		} else {
+			res.status(404).json({ message: err });
+		}
+	});
+});
+app.delete('/products/:id', (req, res) => {
+	const { id } = req.params;
+	Products.findByIdAndDelete(id, (err) => {
+		if (!err) {
+			res.status(200);
+		} else {
+			res.status(404).json({ message: err });
+		}
+	});
+});
 
-app.put("/products/:id", (req, res) => {
-    const id = req.params.id
+app.post('/products', (req, res) => {
+	const product = new Products({
+		productName: req.body.productName,
+		price: req.body.price,
+		count: req.body.count,
+		src: req.body.src
+	});
+	product.save();
+	res.status(200);
+});
 
-    products = products.filter(x => x.id != id)
+app.put('/products/:id', (req, res) => {
+	const { id } = req.params;
 
-    let update = {
-        id: id,
-        productName: req.body.productName,
-        price: req.body.price,
-        count: req.body.count,
-        src: req.body.src
-    }
-    products.push(update)
-    products.sort((a, b) => a.id - b.id)
-    res.send(products)
-})
-app.listen(PORT, () => {
-    console.log('Server Loading');
-})
+	Products.findByIdAndUpdate(id, req.body, (err, doc) => {
+		if (!err) {
+			res.status(200);
+		} else {
+			res.status(404).json({ message: err });
+		}
+	});
+	res.status(200);
+});
+
+const PORT = process.env.PORT;
+const url = process.env.CONNECTION_URL;
+mongoose.set('strictQuery', true);
+mongoose.connect(url, (err) => {
+	if (!err) {
+		console.log('DB connect');
+		app.listen(PORT, () => {
+			console.log('Server start');
+		});
+	}
+});
